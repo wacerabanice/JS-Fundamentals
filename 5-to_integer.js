454134dd595fae92a330f577d189bb77450e2cdb
Wacera <winniebanice@gmail.com>
2025-09-10 15:24:31 +0300

    Amend int file

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -2,7 +2,7 @@ const firstArg = process.argv[2];
 const num = parseInt(firstArg, 10);
 
 if (isNaN(num)) {
-  console.log(Not a number);
+  console.log("Not a number");
 } else {
-  console.log();
+  console.log(`My number: ${num}`);
 }
