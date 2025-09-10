9dddacbf84890c69758873980011f81253318e77
Wacera <winniebanice@gmail.com>
2025-09-10 18:38:16 +0300

    Amend Task 7

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,1 +1,9 @@
-const tmes = parseInt(process.argv[2], 10); if (isNaan(times)){console.log(Missing number of occurences);}else{for (let i=0; i<times; i++){ console.log(C is fun);}}
+const times = parseInt(process.argv[2], 10);
+
+if (isNaN(times)) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < times; i++) {
+    console.log("C is fun");
+  }
+}
