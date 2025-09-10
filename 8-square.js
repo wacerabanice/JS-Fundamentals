9bbfb303584759ea6b72f97de785e6b0ba4c109d
Wacera <winniebanice@gmail.com>
2025-09-10 18:45:06 +0300

    amend task 8

--- 8-square.js
+++ 8-square.js
@@ -1,9 +1,11 @@
 const size = parseInt(process.argv[2], 10);
 
 if (isNaN(size)) {
-  console.log(Missing size);
+  console.log("Missing size");
 } else if (size > 0) {
-  for (let i = 0; i < size; i++) {
-    console.log(X.repeat(size));
+  let i = 0;
+  while (i < size) {
+    console.log("X".repeat(size));
+    i++;
   }
 }
