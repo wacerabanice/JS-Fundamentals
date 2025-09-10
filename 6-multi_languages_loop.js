38c957465c9615d47e164b2058ccd1fd719fb51c
Wacera <winniebanice@gmail.com>
2025-09-10 18:34:57 +0300

    Amend task 6

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,7 @@
-const languages = [C is fun, Python is cool, JavaScript is amazing];
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let i = 0;
 
-for (let i = 0; i < languages.length; i++) {
+while (i < languages.length) {
   console.log(languages[i]);
+  i++;
 }
